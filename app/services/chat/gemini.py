1ae9d133e2a7c18fcabdca5d55221c020703a166
bikraabn1 <bikraabn@gmail.com>
2026-04-08 16:32:10 +0700

    update response stream from plain text into JSON

--- app/services/chat/gemini.py
+++ app/services/chat/gemini.py
@@ -1,16 +1,19 @@
+import json
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel(model_name="gemini-2.5-flash-lite")
 
-async def gemini_stream_chat(prompt : str) :
+async def gemini_stream_chat(prompt: str):
     response = await model.generate_content_async(prompt, stream=True)
     
-    async for chunk in response :
+    async for chunk in response:
         if chunk.text:
-            yield chunk.text
+            json_data = json.dumps({"text": chunk.text})
+            
+            yield f"data: {json_data}\n\n"
